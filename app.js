772a8ac3550472d4c152451ba048e09dbafbde65
tthanhvu002 <81304749+tthanhvu002@users.noreply.github.com>
2022-06-18 11:33:24 +0700

    pj:

---
+++ app.js
@@ -0,0 +1,28 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
+const menus = document.querySelectorAll('.menu li')
+Array.from(menus).map(item => {
+    item.onclick = () => {
+        item.querySelector('.sub-menu').classList.toggle('active')
+    }
+})
