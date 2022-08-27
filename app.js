a7c304f2ee691757e893e131df566c89c5c5274d
tthanhvu002 <81304749+tthanhvu002@users.noreply.github.com>
2022-08-27 22:16:23 +0700

    gh repo push tunguyen8425/LPWW

--- app.js
+++ app.js
@@ -1,49 +1,35 @@
-
-
-
-const menus = document.querySelectorAll('.menu > li')
-Array.from(menus).map(item => {
-    item.onclick = () => {
-        item.querySelector('.sub-menu').classList.toggle('active')
-        console.log(1);
-        Array.from(menus).map(i => {
-          if(!i.isSameNode(item)){
-            i.classList.add('opacity-0')
-
-          }
-        })
-    }
-    window.addEventListener('click', function(e){   
-      if (item.contains(e.target)){
-        // Clicked in box
-    
-      } else{
-        // Clicked outside the box
-        item.querySelector('.sub-menu').classList.remove('active')
-        
+const menus = document.querySelectorAll(".menu > li");
+Array.from(menus).map((item) => {
+  item.onclick = () => {
+    item.querySelector(".sub-menu").classList.toggle("active");
+    console.log(1);
+    Array.from(menus).map((i) => {
+      if (!i.isSameNode(item)) {
+        i.classList.add("opacity-0");
       }
     });
-})
+  };
+  window.addEventListener("click", function (e) {
+    if (item.contains(e.target)) {
+      // Clicked in box
+    } else {
+      // Clicked outside the box
+      item.querySelector(".sub-menu").classList.remove("active");
+    }
+  });
+});
 
-const navMbBtn = document.querySelectorAll('.nav-mobile-btn')
-const header = document.querySelectorAll('.header')
-const close = document.querySelectorAll('.close-btn')
+const navMbBtn = document.querySelectorAll(".nav-mobile-btn");
+const header = document.querySelectorAll(".header");
+const close = document.querySelectorAll(".close-btn");
 console.log(navMbBtn);
-Array.from(navMbBtn).map((item,index) => {
+Array.from(navMbBtn).map((item, index) => {
   item.onclick = () => {
-    header[index].classList.add('active')
-
-  }
-})
-Array.from(close).map((item,index) => {
+    header[index].classList.add("active");
+  };
+});
+Array.from(close).map((item, index) => {
   item.onclick = () => {
-    header[index].classList.add('active')
-
-  }
-})
-
-
-
-
-
-
+    header[index].classList.add("active");
+  };
+});
